e8682c7381a3b7f9fe02cdaaa113297813aee918
mandilakhemali919-dotcom <mandilakhemali919@gmail.com>
2026-03-04 14:50:06 +0200

    Update Template.java

--- Practical-14/Template.java
+++ Practical-14/Template.java
@@ -20,4 +20,25 @@ public class TemplateFunctional {
         }
     }
 
-  @FunctionalTemplateShuffled
+    @FunctionalInterface
+    interface HashTable {
+        void insert(String key, String value);
+        String lookup(String key);
+    }
+
+    public static void main(String[] args) {
+        int m = 1_000_000;
+        
+        // Create data using functional approach
+        Pair[] data = createShuffledData(m);
+        
+        int[] alphas = {75, 80, 85, 90, 95};
+        int repetitions = 30;
+        int queryCount = 100_000;
+
+        printHeader();
+
+        Arrays.stream(alphas)
+                .mapToObj(alpha -> new TestConfig(alpha, (alpha * m) / 100))
+                .forEach(config -> runTest(config, data, queryCount, repetitions));
+    }
