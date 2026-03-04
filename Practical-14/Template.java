4745b945495805eab7e6ccd09822c236e6dbc4cd
mandilakhemali919-dotcom <mandilakhemali919@gmail.com>
2026-03-04 14:44:04 +0200

    Update Template.java

--- Practical-14/Template.java
+++ Practical-14/Template.java
@@ -2,4 +2,22 @@ import java.util.*;
 import java.util.function.Supplier;
 
 public class Template{
-  
+
+  package Practical_14;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+public class TemplateFunctional {
+    
+    static class Pair {
+        final String key;
+        final String value;
+        
+        Pair(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+  @FunctionalTemplateShuffled
