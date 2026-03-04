2a93c35fafbcb82529e09526172f057f75f792f6
mandilakhemali919-dotcom <mandilakhemali919@gmail.com>
2026-03-04 14:56:59 +0200

    Update Template.java

--- Practical-14/Template.java
+++ Practical-14/Template.java
@@ -1,9 +1,4 @@
-import java.util.*;
-import java.util.function.Supplier;
-
-public class Template{
-
-  package Practical_14;
+ package Practical_14;
 
 import java.util.*;
 import java.util.function.Supplier;
@@ -42,3 +37,39 @@ public class TemplateFunctional {
                 .mapToObj(alpha -> new TestConfig(alpha, (alpha * m) / 100))
                 .forEach(config -> runTest(config, data, queryCount, repetitions));
     }
+  
+
+    private static Pair[] createShuffledData(int m) {
+        List<Integer> keys = new ArrayList<>(m);
+        for (int i = 1; i <= m; i++) keys.add(i);
+        Collections.shuffle(keys);
+        
+        Pair[] data = new Pair[m];
+        for (int i = 0; i < m; i++) {
+            data[i] = new Pair(keys.get(i).toString(), Integer.toString(i + 1));
+        }
+        return data;
+    }
+
+    private static void runTest(TestConfig config, Pair[] allData, 
+                                int queryCount, int repetitions) {
+        Pair[] usedData = Arrays.copyOfRange(allData, 0, config.n());
+        
+        // Create and initialize tables
+        HashTable openTable = createAndPopulateTable(
+                () -> new openHash(allData.length), usedData);
+        HashTable chainTable = createAndPopulateTable(
+                () -> new chainHash(allData.length), usedData);
+        
+        String[] queries = generateQueries(usedData, queryCount);
+        
+        // Run benchmarks
+        double openAvg = benchmarkTable(openTable, queries, repetitions);
+        double chainAvg = benchmarkTable(chainTable, queries, repetitions);
+        
+        printResult(config.alphaPercent(), config.n(), openAvg, chainAvg);
+    }
+
+                
+
+    
