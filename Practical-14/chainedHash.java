e3aa9e6ceb7cfe80162e5c2c5127123cbb81298b
mandilakhemali919-dotcom <mandilakhemali919@gmail.com>
2026-03-04 15:14:11 +0200

    Update chainedHash.java

--- Practical-14/chainedHash.java
+++ Practical-14/chainedHash.java
@@ -1,1 +1,16 @@
 
+import java.util.*;
+
+public class chainHash {
+    private final List<Map<String, String>> table;
+    private final int m;
+    private int size = 0;
+
+    public chainHash(int m) {
+        this.m = m;
+        this.table = new ArrayList<>(m);
+        for (int i = 0; i < m; i++) {
+            table.add(new LinkedHashMap<>()); // Preserves insertion order
+    
+    }
+
