2957b684a37c138b2ecf5cd5e565e24cc43e9aed
mandilakhemali919-dotcom <mandilakhemali919@gmail.com>
2026-03-11 14:53:26 +0200

    Update Anagrams.java

--- Practical-15/Anagrams.java
+++ Practical-15/Anagrams.java
@@ -5,5 +5,32 @@ public class Anagrams{
 
   public static void main(Strings[] args)
   {
-    if (args.length != 1) {
-      System.out.println("Usage 
+    
+        if (args.length != 1) {
+            System.out.println("Usage: java Anagrams inputfile");
+            System.out.println("You gave: " + Arrays.toString(args));
+            System.exit(1);
+        }
+        
+        String inputfile = args[0];
+        System.out.println("Data file: " + inputfile);
+        
+    
+        Map<String, Integer> D = new HashMap<>();
+        
+        try {
+            // Open file with latin-1 encoding
+            BufferedReader f = new BufferedReader(
+                new InputStreamReader(new FileInputStream(inputfile), "ISO-8859-1"));
+            
+            String line;
+            int linenumber = 0;
+            int lines = 0;
+            
+            // Read line by line (like Python's while line != "")
+            while ((line = f.readLine()) != null) {
+                linenumber++;
+                String[] words = line.split("\\s+");
+                
+            
+      
