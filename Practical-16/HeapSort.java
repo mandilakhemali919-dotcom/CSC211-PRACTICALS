c303890c03fe4540cb4da62d1cfad99abb9b1547
mandilakhemali919-dotcom <mandilakhemali919@gmail.com>
2026-03-18 14:38:46 +0200

    Update HeapSort.java

--- Practical-16/HeapSort.java
+++ Practical-16/HeapSort.java
@@ -1,2 +1,14 @@
 import java.util.*;
 import java.io.*
+
+public class TryHeapSort{
+  private static long startTime;
+  private static long endTime;
+
+public static void main(String[] args)
+  {
+    if (args.length != 1)
+    {
+      System.out.println("Usage: java TryHeapSort inputfile");
+      System.out.println("You gave: " + inputfile);
+      
