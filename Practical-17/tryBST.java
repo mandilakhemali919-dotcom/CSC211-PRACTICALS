95c1c4a20a07414e654541a9302b50fd3c90039c
mandilakhemali919-dotcom <mandilakhemali919@gmail.com>
2026-03-25 14:48:56 +0200

    Update tryBST.java

--- Practical-17/tryBST.java
+++ Practical-17/tryBST.java
@@ -7,6 +7,22 @@ public class tryBST{
     root = null;
   }
 
+  public void insert(int data) {
+            root = insertRec(root, data);
+        }
+
+        private tNode insertRec(tNode root, int data) {
+            if (root == null) {
+                root = new tNode(data);
+                return root;
+            }
+
+            if (data < root.data) {
+                root.left = insertRec(root.left, data);
+            } else if (data > root.data) {
+                root.right = insertRec(root.right, data);
+            }
+
   
 
   
